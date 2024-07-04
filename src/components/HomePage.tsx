f8ec7e602aff374b0ce8a5c50514874a94a9cbc1
Alidier <artiom.alieksieiu@gmail.com>
2024-07-04 20:42:37 +0300

    PLS

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Book } from '../types/Book';
 import { GOOGLE_BOOKS_API_URL } from '../constants/api';
+import { buildGoogleBooksApiUrl } from '../utils/apiUtils';
+import { processBookData } from '../utils/dataUtils';
 
 interface HomePageProps {
   query: string;
@@ -40,42 +42,25 @@ const HomePage: React.FC<HomePageProps> = ({
 
   const searchBooks = async (reset = false) => {
     try {
-      let url = `${GOOGLE_BOOKS_API_URL}?q=${query}`;
-      if (category !== 'all') {
-        url += `+subject:${category}`;
-      }
-      url += `&orderBy=${sort}`;
-      url += `&startIndex=${reset ? 0 : (currentPage - 1) * itemsPerPage}`;
-      url += `&maxResults=${itemsPerPage}`;
+      const url = buildGoogleBooksApiUrl(
+        query,
+        category,
+        sort,
+        reset ? 0 : (currentPage - 1) * itemsPerPage,
+        itemsPerPage
+      );
       const response = await fetch(url);
       const data = await response.json();
-      if (data.items) {
-        const uniqueBooks = removeDuplicates(data.items);
-        setBooks((prevBooks) => (reset ? uniqueBooks : [...prevBooks, ...uniqueBooks]));
-        setTotalBooks(data.totalItems || 0);
-        setCurrentPage(reset ? 2 : currentPage + 1);
-        setIsClicked(false);
-      } else {
-        setBooks([]);
-        setTotalBooks(0);
-        setIsClicked(false);
-      }
+      const { uniqueBooks, totalItems } = processBookData(data);
+      
+      setBooks((prevBooks) => (reset ? uniqueBooks : [...prevBooks, ...uniqueBooks]));
+      setTotalBooks(totalItems);
+      setCurrentPage(reset ? 2 : currentPage + 1);
+      setIsClicked(false);
     } catch (error) {
       console.log('Error fetching books', error);
       setIsClicked(false);
     }
-  };
-
-  const removeDuplicates = (newBooks: Book[]) => {
-    const ids = new Set<string>();
-    const uniqueBooks: Book[] = [];
-    newBooks.forEach((book) => {
-      if (!ids.has(book.id)) {
-        ids.add(book.id);
-        uniqueBooks.push(book);
-      }
-    });
-    return uniqueBooks;
   };
 
   const handleLoadMore = () => {
